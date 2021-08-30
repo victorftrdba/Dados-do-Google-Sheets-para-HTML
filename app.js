e71cc1d5e9a7d49b629e725828e83173edd3accf
Victor Nogueira <victornogu80@gmail.com>
2021-08-30 20:47:49 -0300

    Fixes

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ fetch(`https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:j
         const json = JSON.parse(text.substr(47).slice(0, -2));
         const tabela = json.table.rows;
         for (let i = 0; i < tabela.length; i++) {
-            document.querySelector('#dados').innerHTML += `<div style="padding:10px;box-shadow:0 0 2px .5px black">${tabela[i].c[0].v}, ${tabela[i].c[1].v}, ${tabela[i].c[2].v}</div>`
+            document.querySelector('#dados').innerHTML += `<div style="display:flex;align-items:center;justify-content:center;width:300px;padding:10px;box-shadow:0 0 2px .5px black">${tabela[i].c[0].v}, ${tabela[i].c[1].v}, ${tabela[i].c[2].v}</div>`
         }
     })
